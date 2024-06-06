58d0717e5a4ceedb258232a11a74ae325f99606c
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-06-06 17:27:53 -0300

    Update files

--- 01-items-list/src/App.jsx
+++ 01-items-list/src/App.jsx
@@ -1,4 +1,10 @@
-import "./App.css";
+const mugs = [
+  {
+    white: { img: "./public/imgs/white.jpg", price: 12.49 },
+    black: { img: "./public/imgs/black.jpg", price: 14.49 },
+    brown: { img: "./public/imgs/brown.jpg", price: 10.49 },
+  },
+];
 
 function App() {
   return (
@@ -13,7 +19,7 @@ function Selections() {
   return (
     <div>
       <label>Select your Coffee Mug</label>
-      <img src="./public/imgs/black_mug.jpg" alt="white coffee mug" />
+      <img src="imgs/white_mug.jpg" alt="white coffee mug" />
     </div>
   );
 }
