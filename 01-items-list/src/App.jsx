580f6ca876eb2f863a2f5ec299ddff3cb0b912cb
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-06-07 11:02:48 -0300

    Update App.jsx and styles.css

--- 01-items-list/src/App.jsx
+++ 01-items-list/src/App.jsx
@@ -1,14 +1,26 @@
+import React, { useState } from "react";
+
 const mugs = [
   {
-    white: { img: "./public/imgs/white.jpg", price: 12.49 },
-    black: { img: "./public/imgs/black.jpg", price: 14.49 },
-    brown: { img: "./public/imgs/brown.jpg", price: 10.49 },
+    color: "White",
+    img: "imgs/white_mug.jpg",
+    price: 12.49,
+  },
+  {
+    color: "Brown",
+    img: "imgs/brown_mug.jpg",
+    price: 12.49,
+  },
+  {
+    color: "Black",
+    img: "imgs/black_mug.jpg",
+    price: 12.49,
   },
 ];
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Selections />
       <ShoppingList />
     </div>
@@ -16,16 +28,49 @@ function App() {
 }
 
 function Selections() {
+  const [selectedColor, setSelectedColor] = useState(mugs[0].color);
+
+  function handleColorSelection(event) {
+    setSelectedColor(event.target.value);
+  };
+
+  const selectedMug = mugs.find((mug) => mug.color === selectedColor);
+  // console.log(selectedMug);
+
   return (
-    <div>
-      <label>Select your Coffee Mug</label>
-      <img src="imgs/white_mug.jpg" alt="white coffee mug" />
+    <div className="selections">
+      <div>
+        <p>Select your Coffee Mug</p>
+        <img
+          src={selectedMug.img}
+          alt={`${selectedColor} Coffee Mug`}
+          className="mug-selection"
+        />
+      </div>
+
+      <label>Color</label>
+      <select value={selectedColor} onChange={handleColorSelection}>
+        {mugs.map((mug, i) => (
+          <option value={mug.color} key={i}>
+            {mug.color}
+          </option>
+        ))}
+      </select>
+
+      <br />
+
+      <label>Quantity</label>
+      <input type="number" min="1" />
+
+      <br />
+
+      <button>Add to Cart</button>
     </div>
   );
 }
 
 function ShoppingList() {
-  return <div></div>;
+  return <div className="shopping-list"></div>;
 }
 
 export default App;
