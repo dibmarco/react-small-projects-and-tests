da59dd3ed499a2a8a895c8e6c118c882054c7532
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-06-08 17:37:58 -0300

    Update App.jsx

--- 01-items-list/src/App.jsx
+++ 01-items-list/src/App.jsx
@@ -7,11 +7,11 @@ const mugs = {
   },
   brown: {
     img: "imgs/brown_mug.jpg",
-    price: 12.49,
+    price: 13.49,
   },
   black: {
     img: "imgs/black_mug.jpg",
-    price: 12.49,
+    price: 15.49,
   },
 };
 
@@ -25,13 +25,31 @@ function App() {
 }
 
 function Selections() {
-  const [selectedColor, setSelectedColor] = useState('white');
+  const [selectedColor, setSelectedColor] = useState("white");
+  const [quantity, setQuantity] = useState(1);
+  const [price, setPrice] = useState(mugs["white"].price);
 
-  function handleColorChange(event) {
-    setSelectedColor(event.target.value);
-  };
+  function handleColorChange(color) {
+    setSelectedColor(color);
+    setQuantity(1);
+    setPrice(mugs[color].price);
+  }
 
   const selectedMug = mugs[selectedColor];
+
+  function handleQuantity(value) {
+    setQuantity(value);
+  }
+
+  function handleAddToCart() {
+    const mugSelections = {
+      color: selectedColor,
+      quantity: quantity,
+      price: quantity * price,
+    };
+
+    // console.log(mugSelections);
+  }
 
   return (
     <div className="selections">
@@ -45,7 +63,7 @@ function Selections() {
       </div>
 
       <label>Color</label>
-      <select value={selectedColor} onChange={handleColorChange}>
+      <select value={selectedColor} onChange={(e) => handleColorChange(e.target.value)}>
         {/* Object.keys() creates an array containing the keys of a given object */}
         {Object.keys(mugs).map((color, i) => (
           <option value={color} key={i}>
@@ -57,11 +75,20 @@ function Selections() {
       <br />
 
       <label>Quantity</label>
-      <input type="number" min="1" />
+      <select
+        value={quantity}
+        onChange={(e) => handleQuantity(Number(e.target.value))}
+      >
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
 
       <br />
 
-      <button>Add to Cart</button>
+      <button onClick={() => handleAddToCart()}>Add to Cart</button>
     </div>
   );
 }
