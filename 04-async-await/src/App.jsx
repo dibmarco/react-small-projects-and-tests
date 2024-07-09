9386e9fbaaf4458474c194c1bca261578d465bcd
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-07-09 13:53:14 -0300

    Update App.jsx

--- 04-async-await/src/App.jsx
+++ 04-async-await/src/App.jsx
@@ -1,31 +1,55 @@
 import { useState, useEffect } from "react";
 
-const KEY = "48e806e1";
-const movieString = "zodiac";
-
 function App() {
-  const [movie, setMovie] = useState(null);
+  const [quote, setQuote] = useState("");
+  const [nextQuote, setNextQuote] = useState(0); // this only triggers a new rendeding of the app
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(
     () =>
       async function getQuote() {
-        const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${movieString}`
-        );
-        const data = await res.json();
-        // console.log(data);
-        const zodiac = await data.Search.at(0);
-        setMovie(zodiac);
+        try {
+          const res = await fetch("https://type.fit/api/quotes");
+          const quotes = await res.json();
+
+          const maxQuotes = quotes.length;
+
+          const randomQuote = function () {
+            return Math.floor(Math.random() * maxQuotes) + 1;
+          };
+
+          setQuote(quotes[randomQuote()]);
+        } catch (error) {
+          console.error("Error fetching quote:", error);
+          setErrorMessage("Error fetching quote.");
+        }
       },
-    []
+    [nextQuote]
   );
 
-  console.log(movie);
+  function handleNextQuote() {
+    setNextQuote(nextQuote + 1);
+  }
+
+  function handleTweet() {
+    const text = quote.text;
+    const author = quote.author;
+
+    window.open(`https://twitter.com/intent/tweet?text=${text author}`, "Share Quote", "width=600, height=300");
+  }
 
   return (
     <div>
-      <h1>{movie.Title} | {movie.Year}</h1>
-      <img src={movie.Poster} alt="zodiac poster" />
+      {!errorMessage ? (
+        <div>
+          <h1>{quote.text}</h1>
+          <p>{quote.author}</p>
+        </div>
+      ) : (
+        errorMessage
+      )}
+      <button onClick={handleNextQuote}>Next Quote</button>
+      <button onClick={handleTweet}>Tweet Quote</button>
     </div>
   );
 }
