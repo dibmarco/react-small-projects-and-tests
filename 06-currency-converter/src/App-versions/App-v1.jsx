e77b930fa48d2d7252766dda63e9a946b579dc6b
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-07-22 15:08:17 -0300

    Update files

--- 06-currency-converter/src/App-versions/App-v1.jsx
+++ 06-currency-converter/src/App-versions/App-v1.jsx
@@ -5,6 +5,7 @@ function App() {
   const [from, setFrom] = useState("USD");
   const [to, setTo] = useState("BRL");
   const [convertedAmount, setConvertedAmount] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -14,6 +15,7 @@ function App() {
     }
 
     async function fetchRates() {
+      setIsLoading(true);
       setError(null);
       try {
         const res = await fetch(
@@ -25,10 +27,14 @@ function App() {
         }
 
         const data = await res.json();
+        /* console.log(data); */
         const [rate] = Object.values(data.rates);
+        /* console.log(rate); */
         setConvertedAmount(rate.toFixed(2));
       } catch {
         setError("Something went wrong. Try again.");
+      } finally {
+        setIsLoading(false);
       }
     }
     fetchRates();
@@ -40,11 +46,13 @@ function App() {
         type="number"
         value={amount}
         onChange={(e) => setAmount(e.target.value)}
+        disabled={isLoading}
       />
       <select
         name="from"
         id="from"
         onChange={(e) => setFrom(e.target.value)}
+        disabled={isLoading}
       >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
@@ -55,11 +63,12 @@ function App() {
         name="to"
         id="to"
         onChange={(e) => setTo(e.target.value)}
+        disabled={isLoading}
       >
         <option value="BRL">BRL</option>
         <option value="EUR">EUR</option>
         <option value="USD">USD</option>
-        <option value="GBP">GBP</option>
+        <option value="EUR">GBP</option>
       </select>
       <div className="result">{!error ? convertedAmount : error}</div>
     </div>
