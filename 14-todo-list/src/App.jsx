a729db79a5f4a4e9235094bb0ab719fc62a9f7c0
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-10-21 16:03:37 -0300

    Update files

--- 14-todo-list/src/App.jsx
+++ 14-todo-list/src/App.jsx
@@ -9,9 +9,26 @@ function App() {
   }
 
   function addTaskToList(task) {
-    setTaskList((prevTasks) => [...prevTasks, task]);
-    setInput("");
+    if (task.trim() === "") {
+      alert("Enter a task.");
+    } else {
+      setTaskList((prevTasks) => [
+        { text: task.toLowerCase(), done: false },
+        ...prevTasks,
+      ]);
+      setInput("");
+    }
   }
+
+  function markComplete(index) {
+    setTaskList((prevTasks) =>
+      prevTasks.map(
+        (task, i) => (i === index ? { ...task, done: !task.done } : task) // Toggle the done status of the clicked task
+      )
+    );
+  }
+
+  const remainingTasks = taskList.filter((task) => !task.done).length;
 
   return (
     <div className="App">
@@ -33,8 +50,18 @@ function App() {
       <br />
       <br />
       <div className="task-list">
-        <p>Your tasks ({taskList.length})</p>
-        {taskList.map((task, i) => <p key={i}>{task}</p>)}
+        <p>Your tasks ({remainingTasks})</p>
+        <ul>
+          {taskList.map((task, i) => (
+            <li
+              key={i}
+              onClick={() => markComplete(i)}
+              className={task.done ? "done" : ""}
+            >
+              {task.text}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
