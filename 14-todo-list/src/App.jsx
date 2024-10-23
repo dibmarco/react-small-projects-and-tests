2dddfd91ad1ee0ca982372c30c37a26b060653e1
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-10-23 11:48:58 -0300

    Update files

--- 14-todo-list/src/App.jsx
+++ 14-todo-list/src/App.jsx
@@ -1,162 +1,97 @@
 import { useEffect, useRef, useState } from "react";
 
-const initialTasks = [
+const initialTaskLog = [
   {
-    taskName: "wash car",
-    taskNotes: "",
-    done: true,
+    date: "October 23, 2024",
+    tasks: [
+      {
+        taskName: "wash car",
+        taskNotes: "",
+        done: true,
+      },
+      {
+        taskName: "buy fruits",
+        taskNotes: "grapes, oranges, apples",
+        done: true,
+      },
+    ],
   },
   {
-    taskName: "buy fruits",
-    taskNotes: "grapes, oranges, apples",
-    done: false,
+    date: "January 1, 2024",
+    tasks: [
+      {
+        taskName: "dentist appointment",
+        taskNotes: "",
+        done: false,
+      },
+      {
+        taskName: "fix fridge",
+        taskNotes: "be careful",
+        done: false,
+      },
+    ],
   },
 ];
 
 function App() {
-  const [taskName, setTaskName] = useState("");
-  const [taskNotes, setTaskNotes] = useState("");
-  const [taskList, setTaskList] = useState(initialTasks);
+  const date = new Date();
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  const formattedDate = new Intl.DateTimeFormat("en-US", options).format(date);
 
-  const [visibleNoteIndex, setVisibleNoteIndex] = useState(null); // Track notes visibility for each task
 
-  const remainingTasks = taskList.filter((task) => !task.done).length;
-
-  function addTaskName(taskName) {
-    setTaskName(taskName);
-  }
-
-  function addTaskNote(taskNotes) {
-    setTaskNotes(taskNotes);
-  }
-
-  function addTaskToList(taskName, taskNotes) {
-    if (taskName.trim() === "") {
-      alert("Enter a task.");
-    } else {
-      setTaskList((prevTasks) => [
-        {
-          taskName: taskName.toLowerCase(),
-          taskNotes: taskNotes.toLowerCase(),
-          done: false,
-        },
-        ...prevTasks,
-      ]);
-      setTaskName("");
-      setTaskNotes("");
-    }
-  }
-
-  function markComplete(index) {
-    setTaskList((prevTasks) =>
-      prevTasks.map(
-        (task, i) => (i === index ? { ...task, done: !task.done } : task) // Toggle the done status of the clicked task
-      )
-    );
-    setVisibleNoteIndex(null);
-  }
-
-  function toggleNotes(index) {
-    setVisibleNoteIndex(visibleNoteIndex === index ? null : index); // Toggle notes visibility for each task
-  }
+  const [taskLog, setTaskLog] = useState(initialTaskLog);
+  const [todayTaskList, setTodayTaskList] = useState(() =>
+    taskLog.find((today) => today.date === formattedDate)
+  );
 
   return (
     <div className="App">
-      <InputField
-        taskName={taskName}
-        taskNotes={taskNotes}
-        addTaskName={addTaskName}
-        addTaskNote={addTaskNote}
-        addTaskToList={addTaskToList}
-        setVisibleNoteIndex={setVisibleNoteIndex}
-      />
-      <TaskList
-        taskList={taskList}
-        remainingTasks={remainingTasks}
-        markComplete={markComplete}
-        visibleNoteIndex={visibleNoteIndex}
-        toggleNotes={toggleNotes}
-      />
+      <InputField />
+      <TaskLog>
+        <TaskList presentDate={formattedDate} taskLog={taskLog} />
+      </TaskLog>
     </div>
   );
 }
 
-function InputField({
-  taskName,
-  taskNotes,
-  addTaskName,
-  addTaskNote,
-  addTaskToList,
-  setVisibleNoteIndex,
-}) {
+function InputField() {
   const [openForm, setOpenForm] = useState(false);
-  const taskInputRef = useRef();
-
-  // const date = new Date();
-  // const options = { year: "numeric", month: "long", day: "numeric" };
-  // const formattedDate = new Intl.DateTimeFormat("en-US", options).format(date);
-
-  useEffect(() => {
-    if (openForm) {
-      taskInputRef.current?.focus();
-    }
-  }, [openForm]);
+  const [newTask, setNewTask] = useState("");
+  const [notes, setNotes] = useState("");
 
   return (
-    <div className="input-field">
-      <button
-        onClick={() => {
-          setOpenForm(true);
-          setVisibleNoteIndex(null);
-        }}
-      >
-        Create Task +
-      </button>
-      <br />
+    <div>
+      <button onClick={() => setOpenForm(!openForm)}>Create Task</button>
       {openForm && (
         <>
           <br />
+          <br />
           <input
-            ref={taskInputRef}
             type="text"
             id="enter-task"
             name="enter-task"
-            minlength="4"
-            maxlength="40"
+            minLength="4"
+            maxLength="40"
             size="22"
             placeholder="Enter your task"
-            value={taskName}
-            onChange={(e) => addTaskName(e.target.value)}
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
             required
           />
+          <br />
           <br />
           <input
             type="text"
             maxLength="200"
             size="22"
             placeholder="Optional: Add notes"
-            value={taskNotes}
-            onChange={(e) => addTaskNote(e.target.value)}
+            value={notes}
+            onChange={(e) => setNotes(e.target.value)}
           />
           <br />
           <div className="add-cancel-btns">
-            <button
-              onClick={() => {
-                addTaskToList(taskName, taskNotes);
-                setOpenForm(false);
-              }}
-            >
-              Add task
-            </button>
-            <p
-              className="cancel-btn"
-              onClick={() => {
-                setOpenForm(false);
-                setVisibleNoteIndex(null);
-              }}
-            >
-              Cancel
-            </p>
+            <button>Add task</button>
+            <p onClick={() => setOpenForm(false)}>Cancel</p>
           </div>
         </>
       )}
@@ -164,47 +99,29 @@ function InputField({
   );
 }
 
-function TaskList({
-  taskList,
-  remainingTasks,
-  markComplete,
-  visibleNoteIndex,
-  toggleNotes,
-}) {
+function TaskLog({ children }) {
   return (
-    <div className="task-list">
-      <p>
-        Tasks To Do{" "}
-        {remainingTasks === 0 && taskList.length > 0
-          ? "(All done!)"
-          : remainingTasks > 0
-          ? `(${remainingTasks} remaining)`
-          : ""}
-      </p>
-      <ul>
-        {taskList.map((task, i) => (
-          <li key={i} className={task.done ? "done" : ""}>
-            <div>{task.date}</div>
-            <div onClick={() => markComplete(i)}>&#8618; {task.taskName}</div>
-            {task.taskNotes && (
-              <>
-                <span
-                  onClick={() => toggleNotes(i)}
-                  // title={task.taskNotes}
-                  title="View notes"
-                  role="button"
-                  // aria-label={task.taskNotes}
-                  aria-label="View notes"
-                >
-                  🗒️
-                </span>
-                {visibleNoteIndex === i && <div>{task.taskNotes}</div>}
-              </>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div>Your tasks day by day:</div>
+      <div>{children}</div>
+    </>
+  );
+}
+
+function TaskList({ presentDate, taskLog }) {
+  return (
+    <>
+      {taskLog.map((taskList, i) => (
+        <div key={i} className={presentDate === taskList.date ? "" : "expired"}>
+          <p>{taskList.date}</p>
+          <ul>
+            {taskList.tasks.map((task) => (
+              <li>{task.taskName}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>
   );
 }
 
