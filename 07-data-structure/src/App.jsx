99fb2eb4c7903fd0f126a677a5ace5fef9c8f15b
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-10-29 12:20:02 -0300

    Update App.jsx

--- 07-data-structure/src/App.jsx
+++ 07-data-structure/src/App.jsx
@@ -7,15 +7,77 @@ const initialShirts = {
   black: { small: 3, medium: 0 },
 };
 
-
 function App() {
   const [shirts, setShirts] = useState(initialShirts);
+  const [selectedColor, setSelectedColor] = useState("");
+  const [selectedSize, setSelectedSize] = useState("");
+  const [selectedQty, setSelectedQty] = useState("");
+  
+  const colors = [...Object.keys(shirts)];
+  console.log(colors);
 
-  const colors = shirts.flatMap((shirt) => Object.keys(shirt));
+  function availableSizes() {
+    let sizes = [];
+    if (selectedColor) {
+      sizes = [...Object.keys(shirts[selectedColor])];
+    }
+    return sizes;
+  }
+
+  function availableQty() {
+    let qty = [];
+    if (selectedColor && selectedSize) {
+      const quantity = shirts[selectedColor][selectedSize];
+      qty = Array.from({ length: quantity }, (_, i) => i + 1);
+    }
+    return qty;
+  }
 
   return (
     <div className="App">
-      
+      <div className="colors">
+        <label htmlFor="shirt-colors">Color: </label>
+        <select id="colors" onChange={(e) => setSelectedColor(e.target.value)}>
+          <option value="">----</option>
+          {colors.map((color) => (
+            <option value={color} key={color}>
+              {color}
+            </option>
+          ))}
+        </select>
+      </div>
+      <br />
+      <div className="sizes">
+        <label htmlFor="shirt-sizes">Size: </label>
+        <select
+          disabled={!selectedColor}
+          id="sizes"
+          onChange={(e) => setSelectedSize(e.target.value)}
+        >
+          <option value="">----</option>
+          {availableSizes().map((size) => (
+            <option key={size} value={size}>
+              {size}
+            </option>
+          ))}
+        </select>
+      </div>
+      <br />
+      <div className="qty">
+        <label htmlFor="shirt-qty">Quantity: </label>
+        <select
+          disabled={!selectedSize}
+          id="qty"
+          onChange={(e) => setSelectedQty(e.target.value)}
+        >
+          <option value="">----</option>
+          {availableQty().map((qty) => (
+            <option key={qty} value={qty}>
+              {qty}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
