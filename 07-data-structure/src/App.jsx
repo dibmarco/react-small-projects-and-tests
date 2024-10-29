57ad60ed491aa8d5f977f8134945f210f965a139
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-10-29 01:11:23 -0300

    Update App.jsx

--- 07-data-structure/src/App.jsx
+++ 07-data-structure/src/App.jsx
@@ -25,10 +25,25 @@ const initialShirts = [
 
 function App() {
   const [shirts, setShirts] = useState(initialShirts);
+  const [colorIndex, setColorIndex] = useState(-1);
   const [color, setColor] = useState("");
+  const [size, setSize] = useState("");
 
   const colors = shirts.flatMap((shirt) => Object.keys(shirt));
   console.log(colors);
+  console.log(shirts[0]["white"]);
+
+  const getQtyOptions = () => {
+    if (color && size && colorIndex !== -1) {
+      const selectedShirt = shirts[colorIndex][color].find(
+        (shirt) => shirt.size === size
+      );
+      if (selectedShirt) {
+        return Array.from({ length: selectedShirt.qty }, (_, k) => k + 1);
+      }
+    }
+    return [];
+  };
 
   return (
     <div className="App">
@@ -37,24 +52,52 @@ function App() {
         <select
           id="color"
           value={color}
-          onChange={(e) => setColor(e.target.value)}
+          onChange={(e) => {
+            const selectedColor = e.target.value;
+            setColor(selectedColor);
+            const cIndex = colors.findIndex((c) => c === selectedColor);
+            setColorIndex(cIndex);
+            setSize(""); // Reset size when color changes
+          }}
         >
           <option value="">------</option>
-          {colors.map((color, index) => (
-            <option key={index} value={color}>
+          {colors.map((color, i) => (
+            <option key={i} value={color}>
               {color}
             </option>
           ))}
         </select>
       </div>
-
+      <br />
       <div>
         <label htmlFor="size">Size: </label>
-        <select id="size" disabled={!color}>
+        <select
+          id="size"
+          value={size}
+          disabled={!color}
+          onChange={(e) => {
+            const selectedSize = e.target.value;
+            setSize(selectedSize);
+          }}
+        >
           <option value="">------</option>
-          {colors.map((color, index) => (
-            <option key={index} value={color}>
-              {color}
+          {color &&
+            colorIndex !== -1 &&
+            shirts[colorIndex][color].map((sizeObj, j) => (
+              <option key={j} value={sizeObj.size}>
+                {sizeObj.size}
+              </option>
+            ))}
+        </select>
+      </div>
+      <br />
+      <div>
+        <label htmlFor="qty">Quantity: </label>
+        <select id="qty" disabled={!size}>
+          <option value="">------</option>
+          {getQtyOptions().map((qty, k) => (
+            <option key={k} value={qty}>
+              {qty}
             </option>
           ))}
         </select>
