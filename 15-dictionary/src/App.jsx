8f65992e7b86caa07fd0d7a8266ba62c05823865
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-10-31 22:12:42 -0300

    Update files

--- 15-dictionary/src/App.jsx
+++ 15-dictionary/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { InputField } from "./components/InputField";
 
 function App() {
   const [query, setQuery] = useState("");
@@ -12,6 +13,12 @@ function App() {
     queryRef.current?.focus();
   }, []);
 
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      handleDefinition(query);
+    }
+  }
+
   function handleDefinition(query) {
     async function fetchWord() {
       if (query.trim() === "") {
@@ -22,13 +29,16 @@ function App() {
       setQuery("");
       setError(null);
       setIsLoading(true);
+
       try {
         const res = await fetch(
           `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`
         );
+
         if (!res.ok) {
           throw new Error("No Definitions Found");
         }
+
         const [data] = await res.json();
         console.log(data);
 
@@ -44,67 +54,61 @@ function App() {
     fetchWord();
   }
 
-  function handleKeyDown(e) {
-    if (e.key === "Enter") {
-      handleDefinition(query);
-    }
-  }
-
   return (
     <div className="App">
-      <div className="input-section">
-        <input
-          type="text"
-          spellCheck={true}
-          value={query}
-          placeholder="Enter a word"
-          ref={queryRef}
-          onChange={(e) => setQuery(e.target.value)}
-          onKeyDown={(e) => handleKeyDown(e)}
-        />
-        <button onClick={() => handleDefinition(query.toLocaleLowerCase())}>
-          Search
-        </button>
-      </div>
-      {isLoading && <p>Searching...</p>}
-      {!isLoading && !error && definition && (
-        <div className="definition-section">
-          <p style={{ fontWeight: "600" }}>{definition.word.toUpperCase()}</p>
-          <p>{definition.phonetic}</p>
-          {definition.meanings.map((meaning, i) => (
-            <div key={i}>
-              <p style={{ fontWeight: "600", marginTop: "10px" }}>
-                {meaning.partOfSpeech}
-              </p>
-              <ul>
-                {meaning.definitions.map((def, j) => (
-                  <li key={j}>{def.definition}</li>
-                ))}
-              </ul>
-              {meaning.synonyms.length > 0 && (
-                <p>
-                  Synonyms:{" "}
-                  {meaning.synonyms.map((synonym) => (
-                    <span
-                      style={{ cursor: "pointer" }}
-                      onClick={() => handleDefinition(synonym)}
-                    >
-                      {synonym}
-                      {" / "}
-                    </span>
-                  ))}
+      <InputField
+        value={query}
+        query={query}
+        queryRef={queryRef}
+        setQuery={setQuery}
+        handleKeyDown={handleKeyDown}
+        handleDefinition={handleDefinition}
+      />
+      <div className="definition-field">
+        {isLoading && (
+          <div className="loading">
+            <p>Searching...</p>
+          </div>
+        )}
+        {!isLoading && error && (
+          <div className="error-message">
+            <p>{error.message}</p>
+          </div>
+        )}
+        {!isLoading && !error && definition && (
+          <div className="definition">
+            <p style={{ fontWeight: "600" }}>{definition.word.toUpperCase()}</p>
+            <p>{definition.phonetic}</p>
+            {definition.meanings.map((meaning, i) => (
+              <div key={i}>
+                <p style={{ fontWeight: "600", marginTop: "10px" }}>
+                  {meaning.partOfSpeech}
                 </p>
-              )}
-            </div>
-          ))}
-        </div>
-      )}
-
-      {!isLoading && error && (
-        <div>
-          <p>{error.message}</p>
-        </div>
-      )}
+                <ul>
+                  {meaning.definitions.map((def, j) => (
+                    <li key={j}>{def.definition}</li>
+                  ))}
+                </ul>
+                {meaning.synonyms.length > 0 && (
+                  <p>
+                    Synonyms:{" "}
+                    {meaning.synonyms.map((synonym) => (
+                      <span
+                        key={synonym}
+                        style={{ cursor: "pointer" }}
+                        onClick={() => handleDefinition(synonym)}
+                      >
+                        {synonym}
+                        {" / "}
+                      </span>
+                    ))}
+                  </p>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ 15-dictionary/src/components/InputField.jsx
@@ -0,0 +1,26 @@
+function InputField({
+  query,
+  queryRef,
+  setQuery,
+  handleKeyDown,
+  handleDefinition,
+}) {
+  return (
+    <div className="input-field">
+      <input
+        type="text"
+        spellCheck={true}
+        value={query}
+        placeholder="Enter a word"
+        ref={queryRef}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={(e) => handleKeyDown(e)}
+      />
+      <button onClick={() => handleDefinition(query.toLocaleLowerCase())}>
+        Search
+      </button>
+    </div>
+  );
+}
+
+export default InputField;
