7927c60cf3deb695ae4f442b477734b42575e8db
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-11-01 23:53:19 -0300

    Update DefinitionField.jsx

--- 15-dictionary/src/components/DefinitionField.jsx
+++ 15-dictionary/src/components/DefinitionField.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 function DefinitionField({ isLoading, error, definition, handleDefinition }) {
   const { word } = useParams();
