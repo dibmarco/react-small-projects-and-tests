68bd00303d70ecbcad6c09cb913bcfa6565adcc3
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-11-16 14:04:06 -0300

    Setup project

---
+++ 17-dictionary-2/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect, useRef } from "react";
+import { Routes, Route, useNavigate, useParams } from "react-router-dom";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [word, setWord] = useState("");
+  const inputEl = useRef(null);
+
+  return (
+    <div className="App">
+      <QueryField
+        inputEl={inputEl}
+        query={query}
+        setQuery={setQuery}
+        setWord={setWord}
+      />
+      <Routes>
+        <Route
+          path="/:wordDefinition"
+          replace
+          element={<DefinitionField setWord={setWord} />}
+        />
+      </Routes>
+    </div>
+  );
+}
+
+function QueryField({ inputEl, query, setQuery, setWord }) {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <input
+        type="text"
+        value={query}
+        ref={inputEl}
+        placeholder="Enter a word"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          setWord(query);
+          setQuery("");
+          navigate(`/${query}`);
+        }}
+      >
+        Search
+      </button>
+    </>
+  );
+}
+
+function DefinitionField({ setWord }) {
+  const { wordDefinition } = useParams();
+
+  useEffect(() => {
+    setWord(wordDefinition);
+    console.log(wordDefinition);
+  }, [wordDefinition, setWord]);
+
+  return <div>{wordDefinition}</div>;
+}
+
+export default App;
