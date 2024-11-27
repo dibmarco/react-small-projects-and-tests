0f65cecfc69a658595f3fc9a0964fb850924ff3c
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-11-27 20:38:01 -0300

    Update useFetchDefinition.js

--- 17-dictionary-2/src/hooks/useFetchDefinition.js
+++ 17-dictionary-2/src/hooks/useFetchDefinition.js
@@ -1,33 +1,38 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect, useRef } from 'react';
 
 function useFetchDefinition(wordToFetch) {
   const [word, setWord] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const previousWord = useRef(null);
 
   const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
   useEffect(() => {
-    async function fetchDefinition() {
-      try {
-        setIsLoading(true);
+    if (wordToFetch !== previousWord.current) {
+      async function fetchDefinition() {
+        try {
+          setIsLoading(true);
 
-        const res = await fetch(`${BASE_URL}${wordToFetch}`);
+          const res = await fetch(`${BASE_URL}${wordToFetch}`);
 
-        if (!res.ok) throw new Error("Failed fetching definition.");
+          if (!res.ok) throw new Error("Failed fetching definition.");
 
-        const [data] = await res.json();
-        console.log(data);
+          const [data] = await res.json();
+          console.log(data);
 
-        setWord(data);
-      } catch (err) {
-        console.error(err.message);
-        setError(err.message);
-      } finally {
-        setIsLoading(false);
+          setWord(data);
+          previousWord.current = wordToFetch; // Update the previous word
+        } catch (err) {
+          console.error(err.message);
+          setError(err.message);
+        } finally {
+          setIsLoading(false);
+        }
       }
+
+      fetchDefinition();
     }
-    if (wordToFetch) fetchDefinition();
   }, [wordToFetch]);
 
   return { word, isLoading, error };
