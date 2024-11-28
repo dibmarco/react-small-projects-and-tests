a6b13c02bdbe5c3146a3cb53763bbe0846fb44d6
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-11-28 14:42:50 -0300

    Update SearchHistory.jsx

--- 17-dictionary-2/src/components/SearchHistory.jsx
+++ 17-dictionary-2/src/components/SearchHistory.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import useQuery from "../hooks/useQuery";
 
 function SearchHistory({ searchedWords }) {
+  const [showMore, setShowMore] = useState(false);
   const { navigateToWord } = useQuery();
 
+  const displayLimit = 5;
+  const wordsToShow = showMore
+    ? searchedWords
+    : searchedWords.slice(0, displayLimit);
+
   return (
-    <div className="flex gap-1">
-      <p className="font-semibold">Search History:</p>
-      <ul>
-        {searchedWords.map((word) => (
+    <div className="flex flex-col sm:flex-row gap-1 sm:max-w-[350px] md:max-w-[600px]">
+      <p className="font-semibold min-w-fit">
+        Search History ({searchedWords.length}):
+      </p>
+      <ul className="flex flex-wrap max-w-full sm:max-w-[calc(100%-4rem)] md:max-w-[calc(600px-4rem)]">
+        {wordsToShow.map((word) => (
           <li
             className="capitalize border-r-2 inline-block mx-0.5 px-1.5 cursor-pointer last:border-r-0"
             onClick={() => navigateToWord(word)}
@@ -16,6 +25,14 @@ function SearchHistory({ searchedWords }) {
             {word}
           </li>
         ))}
+        {searchedWords.length > displayLimit && (
+          <p
+            onClick={() => setShowMore(!showMore)}
+            className="cursor-pointer text-blue-500 hover:underline pl-1.5"
+          >
+            {showMore ? "Less" : "More"}
+          </p>
+        )}
       </ul>
     </div>
   );
