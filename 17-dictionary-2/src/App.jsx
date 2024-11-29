ce4b1ffdb2c025a460fbcd5cb3f8ad49c0e901ae
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-11-29 10:55:26 -0300

    Update App.jsx

--- 17-dictionary-2/src/App.jsx
+++ 17-dictionary-2/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App font-nunito w-[350px] mx-auto md:w-[600px] text-slate-900 flex flex-col h-screen scrollbar-thin">
+    <div className="App font-nunito w-[350px] mx-auto md:w-[600px] text-slate-900 flex flex-col h-screen">
       <QueryField inputEl={inputEl} />
       <Routes>
         <Route
