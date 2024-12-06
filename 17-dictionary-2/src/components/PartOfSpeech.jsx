ade741959740e8ec672e921a58a1561ab16bd415
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-12-06 00:31:51 -0300

    Update PartOfSpeech.jsx

--- 17-dictionary-2/src/components/PartOfSpeech.jsx
+++ 17-dictionary-2/src/components/PartOfSpeech.jsx
@@ -5,8 +5,14 @@ export function PartOfSpeech({ meaning }) {
       <ul className="mb-3 mt-1 list-disc pl-[18px]">
         {meaning.definitions.map((definition, i) => (
           <div key={i}>
-          <li className="w-[320px] sm:w-[400px] md:w-[500px]">{definition.definition}</li>
-          {definition.example && <p className="text-sm pl-3 pr-4 pt-1.5 pb-2 mt-1 mb-2 text-justify text-slate-800 bg-slate-200 max-w-[480px] w-full sm:w-[400px] md:w-[500px] md:ml-4 md:pl-5 rounded-md md:text-base">{definition.example}</p>}
+            <li className="w-[320px] sm:w-[400px] md:w-[500px]">
+              {definition.definition}
+            </li>
+            {definition.example && (
+              <p className="text-sm pl-3 pr-4 pt-1.5 pb-2 mt-1 mb-2 text-justify text-slate-800 bg-slate-200 max-w-[480px] w-full sm:w-[400px] md:w-[500px] md:ml-4 md:pl-5 rounded-md md:text-base">
+                {definition.example}
+              </p>
+            )}
           </div>
         ))}
       </ul>
