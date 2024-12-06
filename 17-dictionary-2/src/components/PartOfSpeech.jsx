23d07486a5d23d0be92b55c20d54aeca24b556da
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-12-06 01:02:58 -0300

    Update PartOfSpeech.jsx

--- 17-dictionary-2/src/components/PartOfSpeech.jsx
+++ 17-dictionary-2/src/components/PartOfSpeech.jsx
@@ -1,18 +1,18 @@
-import { useNavigate } from "react-router-dom";
+import useQuery from "../hooks/useQuery";
 
 export function PartOfSpeech({ meaning }) {
-  const navigate = useNavigate();
+  const { navigateToWord } = useQuery();
 
   function handleDoubleClick(word) {
-    navigate(`/${word}`);
+    navigateToWord(word);
   }
 
-  function renderText(definition) {
-    // Regular expression to remove non-alphabetic characters from the beginning and end
+  function makeWordsClickable(content) {
+    // Regular expression to remove non-alphabetic characters from the beginning and end.
     const cleanWord = (word) => word.replace(/^[^a-zA-Z]+|[^a-zA-Z]+$/g, "");
 
-    // Split the definition into words and wrap each word in a span
-    const words = definition.split(" ");
+    // Split the content into words and wrap each word in a span.
+    const words = content.split(" ");
     return words.map((word, i) => {
       const cleanedWord = cleanWord(word);
       return (
@@ -30,11 +30,11 @@ export function PartOfSpeech({ meaning }) {
         {meaning.definitions.map((definition, i) => (
           <div key={i}>
             <li className="w-[320px] sm:w-[400px] md:w-[500px]">
-              {renderText(definition.definition)}
+              {makeWordsClickable(definition.definition)}
             </li>
             {definition.example && (
               <p className="text-sm pl-3 pr-4 pt-1.5 pb-2 mt-1 mb-2 text-justify text-slate-800 bg-slate-200 max-w-[480px] w-full sm:w-[400px] md:w-[500px] md:ml-4 md:pl-5 rounded-md md:text-base">
-                {renderText(definition.example)}
+                {makeWordsClickable(definition.example)}
               </p>
             )}
           </div>
