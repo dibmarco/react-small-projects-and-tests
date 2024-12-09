229349d6ae18f97cf749d20f6736b06b3767f79b
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-12-09 16:57:25 -0300

    Update useQuery.js

--- 17-dictionary-2/src/hooks/useQuery.js
+++ 17-dictionary-2/src/hooks/useQuery.js
@@ -2,11 +2,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
-const toastOptions = {
-  className: "mt-12 bg-yellow-200 text-black px-4 py-2 rounded-md shadow-lg",
-  duration: 1500,
-};
-
 function useQuery(initialValue = "") {
   const [query, setQuery] = useState(initialValue);
   const navigate = useNavigate();
@@ -14,7 +9,11 @@ function useQuery(initialValue = "") {
   function navigateToWord(word) {
     const searchQuery = word || query;
     if (!searchQuery.trim()) {
-      toast.error("Please type a word!", toastOptions);
+      toast.error("Please type a word!", {
+        className:
+          "mt-12 bg-yellow-200 text-black px-4 py-2 rounded-md shadow-lg",
+        duration: 1500,
+      });
       return;
     }
 
@@ -26,7 +25,11 @@ function useQuery(initialValue = "") {
     if (e.key !== "Enter") return;
 
     if (!query.trim()) {
-      toast.error("Please type a word!", toastOptions);
+      toast.error("Please type a word!", {
+        className:
+          "mt-12 bg-yellow-200 text-black px-4 py-2 rounded-md shadow-lg",
+        duration: 1500,
+      });
       return;
     }
 
